f62d2ed3ebf045b3e1e68a7ab01edc0550642385
TomasFischer69 <109523073+TomasFischer69@users.noreply.github.com>
2022-08-02 19:34:03 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,19 @@
+
+
+let clickOn = document.getElementById('kliker');
+let moveNav = document.getElementById('ul__nav');
+
+
+clickOn.addEventListener('click', () => {
+    
+    moveNav.classList.toggle('ul__klik');
+ 
+    
+});
+
+
+
+
+
+
+
